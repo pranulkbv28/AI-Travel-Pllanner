10750e337ad95dae5b0b921137950cb6d5923d3c
pranulkbv28 <28bvpranaav@gmail.com>
2024-11-10 17:38:57 +0530

    initial commit

---
+++ app/index.tsx
@@ -0,0 +1,13 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+      }}
+    >
+      <Text>Initial Expo App</Text>
+    </View>
+  );
+}
